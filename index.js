2744ccda35da813c6fdd1a0fbaf77835ebdce4bb
Mariela Gonzales <mpgonzales14@gmail.com>
2019-03-03 17:48:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,4 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 
-var funkyFunction = function(){
-
-   return function(){	  return function(){
-    return "FUNKY!"	    return "FUNKY!"
+   
